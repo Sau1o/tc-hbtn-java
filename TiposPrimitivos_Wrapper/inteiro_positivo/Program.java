414874dfa6f16f16d6e33bc7208c3c624f28185b
Saulo Coelho <coelho.saulo@gmail.com>
2024-06-30 15:37:15 -0300

    changed name class

--- TiposPrimitivos_Wrapper/inteiro_positivo/Program.java
+++ TiposPrimitivos_Wrapper/inteiro_positivo/Program.java
@@ -1,52 +1,52 @@
 public class Program {
     public static void main(String[] args) {
         try {
-            InteiroPositivo inteiroPositivo = new InteiroPositivo("1");
-            System.out.println(inteiroPositivo.getValor() + " - " + inteiroPositivo.ehPrimo());
+            Inteiro_positivo Inteiro_positivo = new Inteiro_positivo("1");
+            System.out.println(Inteiro_positivo.getValor() + " - " + Inteiro_positivo.ehPrimo());
         } catch (Exception ex) {
             System.out.println(ex.getMessage());
         }
 
-        InteiroPositivo inteiroPositivo1 = null;
+        Inteiro_positivo Inteiro_positivo1 = null;
 
         try {
-            inteiroPositivo1 = new InteiroPositivo("43");
-            System.out.println(inteiroPositivo1.getValor() + " - " + inteiroPositivo1.ehPrimo());
-        } catch (Exception ex) {
-            System.out.println(ex.getMessage());
-        }
-
-        try {
-            inteiroPositivo1.setValor(-1);
-            System.out.println(inteiroPositivo1.getValor() + " - " + inteiroPositivo1.ehPrimo());
+            Inteiro_positivo1 = new Inteiro_positivo("43");
+            System.out.println(Inteiro_positivo1.getValor() + " - " + Inteiro_positivo1.ehPrimo());
         } catch (Exception ex) {
             System.out.println(ex.getMessage());
         }
 
         try {
-            inteiroPositivo1.setValor(inteiroPositivo1.getValor() + 1);
-            System.out.println(inteiroPositivo1.getValor() + " - " + inteiroPositivo1.ehPrimo());
+            Inteiro_positivo1.setValor(-1);
+            System.out.println(Inteiro_positivo1.getValor() + " - " + Inteiro_positivo1.ehPrimo());
         } catch (Exception ex) {
             System.out.println(ex.getMessage());
         }
 
         try {
-            InteiroPositivo inteiroPositivo = new InteiroPositivo("-3");
-            System.out.println(inteiroPositivo.getValor() + " - " + inteiroPositivo.ehPrimo());
+            Inteiro_positivo1.setValor(Inteiro_positivo1.getValor() + 1);
+            System.out.println(Inteiro_positivo1.getValor() + " - " + Inteiro_positivo1.ehPrimo());
         } catch (Exception ex) {
             System.out.println(ex.getMessage());
         }
 
         try {
-            InteiroPositivo inteiroPositivo = new InteiroPositivo(379);
-            System.out.println(inteiroPositivo.getValor() + " - " + inteiroPositivo.ehPrimo());
+            Inteiro_positivo Inteiro_positivo = new Inteiro_positivo("-3");
+            System.out.println(Inteiro_positivo.getValor() + " - " + Inteiro_positivo.ehPrimo());
         } catch (Exception ex) {
             System.out.println(ex.getMessage());
         }
 
         try {
-            InteiroPositivo inteiroPositivo = new InteiroPositivo(378);
-            System.out.println(inteiroPositivo.getValor() + " - " + inteiroPositivo.ehPrimo());
+            Inteiro_positivo Inteiro_positivo = new Inteiro_positivo(379);
+            System.out.println(Inteiro_positivo.getValor() + " - " + Inteiro_positivo.ehPrimo());
+        } catch (Exception ex) {
+            System.out.println(ex.getMessage());
+        }
+
+        try {
+            Inteiro_positivo Inteiro_positivo = new Inteiro_positivo(378);
+            System.out.println(Inteiro_positivo.getValor() + " - " + Inteiro_positivo.ehPrimo());
         } catch (Exception ex) {
             System.out.println(ex.getMessage());
         }
