86df4565388248fb4e66758a6344d1459198000b
Saulo Coelho <coelho.saulo@gmail.com>
2024-11-21 13:34:24 -0300

    fix error demo

--- TiposPrimitivos_Wrapper/inteiro_positivo/InteiroPositivo.java
+++ TiposPrimitivos_Wrapper/inteiro_positivo/InteiroPositivo.java
@@ -1,8 +1,8 @@
 public class InteiroPositivo {
     
-    private float valor;
+    private int valor;
 
-    public InteiroPositivo(float valor) {
+    public InteiroPositivo(int valor) {
         setValor(valor);
     }
 
@@ -29,7 +29,6 @@ public class InteiroPositivo {
             if (valor % i == 0)
                 return false;
         }
-        // return true;
-        return false;
+        return true;
     }
 }
