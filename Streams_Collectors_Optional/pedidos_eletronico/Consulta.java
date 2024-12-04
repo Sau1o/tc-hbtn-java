5442b00049d5589426b4d62ced08741ef1db2863
Saulo Coelho <coelho.saulo@gmail.com>
2024-12-04 10:24:35 -0300

    update fie ../pedidos_eletronicos/Consulta.java

--- Streams_Collectors_Optional/pedidos_eletronico/Consulta.java
+++ Streams_Collectors_Optional/pedidos_eletronico/Consulta.java
@@ -6,13 +6,13 @@ public class Consulta {
         return pedido.getProdutos()
                      .stream()
                      .filter(p -> p.getCategoria() == CategoriaProduto.LIVRO)
-                     .toList();
+                     .collect(Collectors.toList());
     }
 
     public static Produto obterProdutoMaiorPreco(List<Produto> produtos) {
         return produtos.stream()
                        .sorted(Comparator.comparing(Produto::getPreco).reversed())
-                       .toList()
+                       .collect(Collectors.toList())
                        .get(0);
     }
 
@@ -20,13 +20,13 @@ public class Consulta {
         double precoMinimo) {
         return produtos.stream()
                        .filter(p -> p.getPreco() >= precoMinimo)
-                       .toList();
+                       .collect(Collectors.toList());
     }
 
     public static List<Pedido> obterPedidosComEletronicos(List<Pedido> pedidos) {
         return pedidos.stream()
                       .filter(p -> p.getProdutos().stream()
                                                   .anyMatch(e -> e.getCategoria() == CategoriaProduto.ELETRONICO))
-                      .toList();
+                      .collect(Collectors.toList());
     }
 }
