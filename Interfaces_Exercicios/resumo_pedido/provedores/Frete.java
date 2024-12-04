162e2edacd51d841a19b7280c76c152e1fd97e4d
Saulo Coelho <coelho.saulo@gmail.com>
2024-12-04 17:53:53 -0300

    changed name folder

--- Interfaces_Exercicios/resumo_pedido/provedores/Frete.java
+++ Interfaces_Exercicios/resumo_pedido/provedores/Frete.java
@@ -1,9 +1,5 @@
 package provedores;
 
-import java.text.DecimalFormat;
-import java.text.DecimalFormatSymbols;
-import java.util.Locale;
-
 public class Frete {
     private double valor;
     private TipoProvedorFrete tipoProvedorFrete;
@@ -14,14 +10,7 @@ public class Frete {
     }
 
     public double getValor() {
-        // Configurando o DecimalFormat para usar ponto decimal
-        DecimalFormatSymbols symbols = new DecimalFormatSymbols(Locale.US);
-        symbols.setDecimalSeparator('.');
-        DecimalFormat df = new DecimalFormat("0.00", symbols);
-
-        // Retornando o valor formatado
-        return df.format(valor);
-        // return valor;
+        return valor;
     }
 
     public void setValor(double valor) {
