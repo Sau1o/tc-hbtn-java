3e903bcd8751003d0f2ae2d8a33204aa874c9f8a
Saulo Coelho <coelho.saulo@gmail.com>
2024-12-07 17:27:56 -0300

    fix: added import locale

--- Interfaces_Exercicios/workflow/Program.java
+++ Interfaces_Exercicios/workflow/Program.java
@@ -1,10 +1,11 @@
 import atividades.*;
+import java.util.Locale;
 
 public class Program {
     public static void main(String[] args) {
 
         Locale.setDefault(new Locale("pt", "BR"));
-        
+
         MaquinaWorkflow maquinaWorkflow = new MaquinaWorkflow();
         
         Workflow workflowEncodificarVideos = new Workflow();
