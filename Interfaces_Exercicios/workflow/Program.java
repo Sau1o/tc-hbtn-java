d85950619577f250d0e33bf2301b3e64534cb0ac
Saulo Coelho <coelho.saulo@gmail.com>
2024-12-07 17:15:33 -0300

    fix:added Locale.setDefault(new Locale(pt, BR)); in workflow/Program.java

--- Interfaces_Exercicios/workflow/Program.java
+++ Interfaces_Exercicios/workflow/Program.java
@@ -2,6 +2,9 @@ import atividades.*;
 
 public class Program {
     public static void main(String[] args) {
+
+        Locale.setDefault(new Locale("pt", "BR"));
+        
         MaquinaWorkflow maquinaWorkflow = new MaquinaWorkflow();
         
         Workflow workflowEncodificarVideos = new Workflow();
