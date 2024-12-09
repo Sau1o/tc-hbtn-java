ac2ea90832dafb9739279b057de37720f45127ae
Saulo Coelho <coelho.saulo@gmail.com>
2024-12-09 08:52:21 -0300

    fixed precoComMarkup

--- Lambda/markup/Produto.java
+++ Lambda/markup/Produto.java
@@ -4,11 +4,11 @@ public class Produto {
     private double preco
     private String nome;
     
-    private double percentualMarkUp = 10;
+    private double percentualMarkup = 10;
 
-    public Supplier<Double> precoComMarkUp = () -> this.getPreco() * (1 + (this.percentualMarkUp / 100));
+    public Supplier<Double> precoComMarkup = () -> this.getPreco() * (1 + (this.percentualMarkUp / 100));
 
-    public Consumer<Double> atualizarMarkUp = x -> this.percentualMarkUp = x;
+    public Consumer<Double> atualizarMarkup = x -> this.percentualMarkup = x;
 
     public Produto(double preco, String nome) {
         this.preco = preco;
