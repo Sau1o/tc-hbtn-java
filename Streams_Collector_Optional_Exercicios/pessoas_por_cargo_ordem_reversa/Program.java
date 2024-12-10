caf762b752888ac575c2f8cf82cc32def8d2249f
Saulo Coelho <coelho.saulo@gmail.com>
2024-12-10 09:54:58 -0300

    fixes Program.java

--- Streams_Collector_Optional_Exercicios/pessoas_por_cargo_ordem_reversa/Program.java
+++ Streams_Collector_Optional_Exercicios/pessoas_por_cargo_ordem_reversa/Program.java
@@ -1,10 +1,7 @@
 import java.util.*;
-import java.util.Locale;
 
 public class Program {
     public static void main(String[] args) {
-
-        Locale.setDefault(new Locale("pt", "BR"));
         Pessoa pessoa1 = new Pessoa(1, "John Bannins", "Desenvolvedor", 23, 3200);
         Pessoa pessoa2 = new Pessoa(2, "Derry Collier", "Desenvolvedor", 45, 8600);
         Pessoa pessoa3 = new Pessoa(3, "Mikey Berg", "Product Owner", 51, 11350);
@@ -34,4 +31,3 @@ public class Program {
         System.out.println(pessoas3);
     }
 }
-
